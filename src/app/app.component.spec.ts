560496f64181f24fc01dfddc4b1655aa5428a28e
falalurahman <meet.falalu@gmail.com>
2022-05-07 17:07:19 +0530

    Firebase Setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dijkstra-train-station'`, () => {
+  it(`should have as title 'Dijkstra Railway Stations'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dijkstra-train-station');
+    expect(app.title).toEqual('Dijkstra Railway Stations');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('dijkstra-train-station app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Dijkstra Railway Stations app is running!');
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dijkstra-train-station',
+    appId: '1:141775414779:web:90882152bf2e2c4e65175e',
+    storageBucket: 'dijkstra-train-station.appspot.com',
+    apiKey: 'AIzaSyBIZ30rZBn5gtoHij3o9YdCTn4n70RC6E4',
+    authDomain: 'dijkstra-train-station.firebaseapp.com',
+    messagingSenderId: '141775414779',
+  },
   production: false
 };
 

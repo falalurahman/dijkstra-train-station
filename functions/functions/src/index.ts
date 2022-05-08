54080338aa0a8930784b266bdc2f92acee1a811e
falalurahman <meet.falalu@gmail.com>
2022-05-08 13:50:07 +0530

    Setup Cloud functions

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -10,6 +10,7 @@ export const updateStartStation = functions.https.onRequest(
       const queryText = request.query.startStation;
       const startStation: string | undefined = queryText?.toString();
       if (startStation != undefined) {
+        setStartStation(startStation);
         db.collection("stations").get().then(
             (docSnap: any) => {
               docSnap.forEach((doc: any) => {
@@ -22,8 +23,21 @@ export const updateStartStation = functions.https.onRequest(
                 }
               });
             }
-        );
+        ).catch(() => response.send("Undefined Station"));
       } else {
         response.send("Undefined Station");
       }
     });
+
+const setStartStation = function(newStartStation: string): void {
+  db.collection("startStation").doc("startStation")
+      .set({value: newStartStation});
+};
+
+export const updateDistancesFromStartStation = functions.firestore
+    .document("/startStation/startStation")
+    .onWrite(
+        (change: functions.Change<functions.firestore.DocumentSnapshot>) => {
+          const updatedValue = change.after.data()?.["value"];
+          console.log("Updated Start Station to: " + updatedValue);
+        });

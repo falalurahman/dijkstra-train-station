bd6ae51aa7dfef17f50e4caa2bd41d08636eb065
falalurahman <meet.falalu@gmail.com>
2022-05-29 22:56:40 +0530

    Changing the API to return the route between source and destination

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -13,10 +13,13 @@ const db = admin.firestore();
 
 export const updateStartStation = functions.https.onRequest(
     (request: Request, response: Response) => {
-      const queryText = request.query.startStation;
-      const startStation: string | undefined = queryText?.toString();
-      if (startStation != undefined) {
-        setStartStation(startStation, response);
+      const startStationQueryText = request.query.startStation;
+      const startStation: string | undefined =
+      startStationQueryText?.toString();
+      const endStationQueryText = request.query.endStation;
+      const endStation: string | undefined = endStationQueryText?.toString();
+      if (startStation != undefined && endStation != undefined) {
+        setStartStation(startStation, endStation, response);
       } else {
         response.send("Undefined Station");
       }
@@ -24,19 +27,39 @@ export const updateStartStation = functions.https.onRequest(
 
 const setStartStation = function(
     newStartStation: string,
+    endStation: string,
     response: Response
 ): void {
   db.collection("startStation").doc("startStation")
       .set({value: newStartStation})
       .then(() => {
-        return computeShortestDistanceFromSource(newStartStation);
-      }).then((distanceMap: Map<string, number>) => {
-        const distanceMapSorted = new Map([...distanceMap.entries()]
-            .sort((a, b) => a[1] - b[1]));
-        return Array.from(distanceMapSorted.keys());
+        return computeShortestParentFromSource(newStartStation);
+      }).then((parentMap: Map<string, string>) => {
+        return getShortestRouteFromSourceToDest(parentMap,
+            endStation);
       }).then((shortestDistanceAirportsList: string[]) => {
-        response.json(shortestDistanceAirportsList.slice(1, 6));
+        response.send(shortestDistanceAirportsList.join());
       });
+};
+
+const getShortestRouteFromSourceToDest = function(
+    parentMap: Map<string, string>,
+    destinationAirport: string
+): string[] {
+  const reverseRoute : string[] = [];
+  let parentAirport = destinationAirport;
+  reverseRoute.push(destinationAirport);
+  while (parentAirport != "") {
+    const currentParentAirport = parentAirport;
+    const newParentAirport = parentMap.get(currentParentAirport);
+    if (newParentAirport != undefined && newParentAirport !== "") {
+      parentAirport = newParentAirport;
+      reverseRoute.push(parentAirport);
+    } else {
+      parentAirport = "";
+    }
+  }
+  return reverseRoute.reverse();
 };
 
 export const updateDistancesFromStartStation = functions.firestore
@@ -78,9 +101,9 @@ const updateShortestDistanceFromSourceInFirebase = function(
 };
 
 
-const computeShortestDistanceFromSource = function(
+const computeShortestParentFromSource = function(
     sourceAirport: string
-): Promise<Map<string, number>> {
+): Promise<Map<string, string>> {
   return db.collection("edges").get().then(
       (edgesData: QuerySnapshot<DocumentData>) => {
         const graph = new Map<string, Map<string, number>>();
@@ -100,9 +123,9 @@ const computeShortestDistanceFromSource = function(
               airportSet.add(toAirport);
             });
         const airportList = Array.from(airportSet.values());
-        const distanceMap = runDijkstraAlgorithm(
-            airportList, graph, sourceAirport, 6);
-        return distanceMap;
+        const parentMap = runDijkstraAlgorithmWithPath(
+            airportList, graph, sourceAirport);
+        return parentMap;
       });
 };
 
@@ -184,4 +207,55 @@ const runDijkstraAlgorithm = function(
     }
   }
   return dist;
+};
+
+const runDijkstraAlgorithmWithPath = function(
+    airportList: string[],
+    graph: Map<string, Map<string, number>>,
+    sourceAirport: string
+): Map<string, string> {
+  const dist = new Map<string, number>();
+  const parent = new Map<string, string>();
+  const sptSet = new Set<string>();
+
+  for (let i = 0; i < airportList.length; i++) {
+    const airport = airportList[i];
+    dist.set(airport, Number.MAX_VALUE);
+  }
+
+  dist.set(sourceAirport, 0);
+  parent.set(sourceAirport, "");
+
+  for (let count = 0; count < airportList.length - 1; count++) {
+    const minDistanceAirport = minDistance(dist, sptSet, airportList);
+    sptSet.add(minDistanceAirport);
+
+    // Update dist value of the adjacent
+    // vertices of the picked vertex.
+    for (let v = 0; v < airportList.length; v++) {
+      // Update dist[v] only if is not in
+      // sptSet, there is an edge from u
+      // to v, and total weight of path
+      // from src to v through u is smaller
+      // than current value of dist[v]
+      const adjacentAirport = airportList[v];
+      const alreadyVisited = sptSet.has(adjacentAirport);
+      const edgeDistanceToAdjacentAirport =
+      graph.get(minDistanceAirport)?.get(adjacentAirport) ?? undefined;
+      const distanceToMinDistanceAirport = dist.get(minDistanceAirport) ?? 0;
+      const oldDistanceToAdjacentAirport = dist.get(adjacentAirport) ?? 0;
+
+      if (!alreadyVisited && edgeDistanceToAdjacentAirport != undefined) {
+        const newDistanceToAdjacentAirport = distanceToMinDistanceAirport +
+        edgeDistanceToAdjacentAirport;
+
+        if ( distanceToMinDistanceAirport != Number.MAX_VALUE &&
+          newDistanceToAdjacentAirport < oldDistanceToAdjacentAirport) {
+          dist.set(adjacentAirport, newDistanceToAdjacentAirport);
+          parent.set(adjacentAirport, minDistanceAirport);
+        }
+      }
+    }
+  }
+  return parent;
 };
